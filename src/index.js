36666bcb50f965b88217931580e9c87f5fd9989a
Kevin Dalman <kdalman@absolute.com>
2023-01-02 15:09:22 -0800

    Fix config and deploy HashRouter

--- src/index.js
+++ src/index.js
@@ -11,14 +11,11 @@ import 'styles/normalize.css';
 import sitemap from './sitemap';
 
 // Dynamic basename so sitemap works when hosted at github-pages URL
-const basename = document.querySelector('base')?.getAttribute('href') || undefined;
-// if (basename) basename = `${basename}/`; // Needed?
+const basename = document.querySelector('base')?.getAttribute('href');
 
 // When published on gh-pages, use a HashRouter so child-paths can refreshed without a 404 error
-// TODO: Implement HashRouter when the React Router bug is fixed!
-const createRouter = false && basename ? createHashRouter : createBrowserRouter;
-
-const router = createRouter(sitemap, { basename });
+const createRouter = basename ? createHashRouter : createBrowserRouter;
+const router = createRouter(sitemap);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
