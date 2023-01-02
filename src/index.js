f3e4ab220678a4e184cd9c1bcf29c39ba0286f30
Kevin Dalman <kdalman@absolute.com>
2023-01-02 01:25:07 -0800

    Use BrowserRouter test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
 	createBrowserRouter,
-	createHashRouter,
+	// createHashRouter,
 	RouterProvider,
 } from 'react-router-dom'
 
@@ -11,10 +11,12 @@ import 'styles/normalize.css';
 import sitemap from './sitemap';
 
 // Dynamic basename so sitemap works when hosted at github-pages URL
-const basename = document.querySelector('base')?.getAttribute('href') ?? '/';
+// const basename = document.querySelector('base')?.getAttribute('href') || undefined;
+const basename = undefined;
 
 // When published in a sub-folder, use a hash-router so pages can refresh correctly
-const createRouter = basename.length < 2 ? createBrowserRouter : createHashRouter;
+// const createRouter = basename.length < 2 ? createBrowserRouter : createHashRouter;
+const createRouter = createBrowserRouter;
 
 const router = createRouter(sitemap, { basename });
 
